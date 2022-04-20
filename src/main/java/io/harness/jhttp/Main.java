4478abea19be9ef44f2bc448a84a3a358126abed
Dan F <danf425@gmail.com>
2022-04-20 19:37:03 -0400

    log4j vulnerability

--- src/main/java/io/harness/jhttp/Main.java
+++ src/main/java/io/harness/jhttp/Main.java
@@ -2,9 +2,9 @@ package io.harness.jhttp;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import org.apache.log4j.BasicConfigurator;  
-import org.apache.log4j.LogManager;  
-import org.apache.log4j.Logger;  
+//import org.apache.log4j.BasicConfigurator;  
+//import org.apache.log4j.LogManager;  
+//import org.apache.log4j.Logger;  
 import io.harness.jhttp.server.HttpServer;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
